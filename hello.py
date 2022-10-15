b821fe717aaf4a33647fa0b269bf8fd874a1d4f5
Artyom <artyom.dutchak@gmail.com>
2022-10-15 10:08:42 -0600

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,16 @@
+import yfinance as yf
+import streamlit as st
+import pandas as pd
+
+
+st.write("""#simple stockapp""")
+
+tickerSymbol = 'GOOGL'
+
+tickerData = yf.Ticker(tickerSymbol)
+
+tickerDf = tickerData.history(period="id", start='2010-5-31', end='2020-5-31')
+
+st.line_chart(tickerDf.Close)
+st.line_chart(tickerDf.Volume)
+
